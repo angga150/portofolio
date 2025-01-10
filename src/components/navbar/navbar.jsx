624bf6ba0acbacc5c6009b88655fcb8c335b8846
Angga Haady Wijaya <76893515+angga150@users.noreply.github.com>
2025-01-10 23:33:01 +0700

    Update navbar.jsx

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,7 +6,7 @@ export default function NavBar() {
             <nav className="py-8 flex justify-between items-center fixed w-full right-0 px-6 md:px-10 top-0 bg-background z-40 transition-all duration-500" id="navbar">
                 {/* logo */}
                 <div>
-                    <a className="text-lg font-bold hover:text-secondary" id="cursorHover"> &lt; Rivaldo Tandoko /&gt; </a>
+                    <a className="text-lg font-bold hover:text-secondary" id="cursorHover"> &lt; Angga Haady Wijaya /&gt; </a>
                 </div>
 
                 {/* links */}
